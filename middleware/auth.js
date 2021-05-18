c26d06720c5e4ca6cc663652b53eb049ee287c64
Pavel Srom <srompavel98@gmail.com>
2021-05-18 18:16:11 +0200

    fix(middleware): prettier

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
-const jwt = require("jsonwebtoken")
-const config = require("config")
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 module.exports = async (req, res, next) => {
-  const token = req.header("x-auth-token")
-  if (!token) return res.status(401).send({ message: "Missing token" })
+  const token = req.header('x-auth-token')
+  if (!token) return res.status(401).send({ message: 'Missing token' })
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"))
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
     req.userID = decoded.id
 
     next()
   } catch (err) {
-    return res.status(403).send({ message: "Invalid token" })
+    return res.status(403).send({ message: 'Invalid token' })
   }
 }
